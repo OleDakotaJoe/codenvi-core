308b86e74587a5d063cb1e07045017c22a9d8d46
Steven Sheaves <stevendakotasheaves@gmail.com>
2021-10-30 02:37:04 -0500

    testing

--- environment/environment.go
+++ environment/environment.go
@@ -9,6 +9,7 @@ import (
 func WithEnv(env map[string]string, body *types.Closure) {
 	var initialEnvironment = make(map[string]string)
 	var customEnvironment =  make(map[string]string)
+	fmt.Println("Printing a test line")
 	for key, value := range env {
 		initialValue := os.Getenv(key)
 		if initialValue != "" {
